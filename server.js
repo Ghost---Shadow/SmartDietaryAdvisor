9b76fdc702b250b83b361e155c78f8eb72c0a452
Ghost---Shadow <souradeep.nanda2014@vit.ac.in>
2016-09-01 22:30:33 +0530

    Connected to Neo4j

--- server.js
+++ server.js
@@ -9,13 +9,18 @@ app.listen(80, function () {
 // Host the public folder app
 app.use(express.static('app'));
 
-var apoc = require('apoc');
+var neo4j = require('neo4j-driver').v1;
 
-apoc.query('match (n) return n').exec().then(
-  function (response) {
-    console.log(response);
-  },
-  function (fail) {
-    console.log(fail);
-  }
-);
+var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "foodstuff"));
+var session = driver.session();
+session
+  .run( "CREATE (a:Person {name:'Arthur', title:'King'})" )
+  .then( function()
+  {
+    return session.run( "MATCH (a:Person) WHERE a.name = 'Arthur' RETURN a.name AS name, a.title AS title" )
+  })
+  .then( function( result ) {
+    console.log( result.records[0].get("title") + " " + result.records[0].get("name") );
+    session.close();
+    driver.close();
+  });
