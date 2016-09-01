ba215fd5d88275eeb7211d4958908e61d76c1a6b
Ghost---Shadow <souradeep.nanda2014@vit.ac.in>
2016-09-01 21:42:15 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+// Listen to port 80 http port
+app.listen(80, function () {
+  console.log('Listening for connections');
+});
+
+// Host the public folder app
+app.use(express.static('app'));
+
+var apoc = require('apoc');
+
+apoc.query('match (n) return n').exec().then(
+  function (response) {
+    console.log(response);
+  },
+  function (fail) {
+    console.log(fail);
+  }
+);
