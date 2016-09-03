20e4271118fc6537b5344170b4bfa601288af23c
Ghost---Shadow <souradeep.nanda2014@vit.ac.in>
2016-09-03 10:16:45 +0530

    DB overhaul

--- app/procedureView/procedureView.component.js
+++ app/procedureView/procedureView.component.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('ProcedureController', function ($scope, $http, messagePassing) {    
+app.controller('ProcedureController', function ($scope, $http, messagePassing) {
     $scope.sharedData = messagePassing.sharedData;
 
     $http.get('/procedureView/procedureView.static.json').then(function (response) {
@@ -11,44 +11,41 @@ app.controller('ProcedureController', function ($scope, $http, messagePassing) {
     });
 
     $scope.buildProcedure = function (root) {
-        var topology = $scope.topologicalSort(root, [], []);
-        console.log(topology);
+        var topology = [];
+        $scope.topologicalSort(root, topology, []);
         $scope.procedure = [];
         for (var i = 0; i < topology.length; i++) {
             var step = $scope.buildStepFromHas(topology[i]);
             $scope.procedure.push(step);
         }
-        //console.log($scope.procedure);
     }
 
     $scope.buildStepFromHas = function (has) {
         var step = '';
         step += has.operation + ' ';
-        if (has.quantity != null)
-            step += has.quantity + ' ' + has.unit + ' of ';
+        step += has.quantity == null ? '' : has.quantity + ' ';
+        step += has.unit == null ? '' : has.unit + ' of ';
         var input_ingredient = $scope.ingredients[has.input_ingredient];
-        step += input_ingredient == null ? ' ' : input_ingredient.name + ' to ';
+        step += input_ingredient == null ? ' ' : input_ingredient.name + ' ';
         var input_recipe = $scope.recipes[has.input_recipe];
-        step += input_recipe == null ? ' ' : input_recipe.name + ' ';
-        if (has.procedure != null)
-            step += has.procedure + ' ';
+        step += input_recipe == null || input_recipe.anonymous ? '' : ' to ' + input_recipe.name + ' ';
+        step += has.procedure == null ? '' : has.procedure + ' ';
         var output = $scope.recipes[has.output];
-        step += output == null ? '' : 'to make ' + output.name;
+        step += output == null || output.anonymous ? '' : 'to make ' + output.name;
         return step;
     }
 
     $scope.topologicalSort = function (root, topology, visited) {
         if (root == null) return [];
-        //visited[root] = true;
+        visited[root] = true;
         var edges = $scope.moveDown(root);
-        console.log(edges);
         for (var i = 0; i < edges.length; i++) {
             if (visited[edges[i].input_recipe]) continue;
-            // TODO: FIX BUG
-            topology = ($scope.topologicalSort(edges[i].input_recipe, topology, visited));
+            $scope.topologicalSort(edges[i].input_recipe, topology, visited);
+        }
+        for (var i = 0; i < edges.length; i++) {
             topology.push(edges[i]);
         }
-        return topology;
     }
 
     $scope.moveDown = function (node) {
